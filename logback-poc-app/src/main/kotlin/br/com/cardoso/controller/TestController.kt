b1a63b62ddf3c919b304487f5f77c29049ddecfb
cardoso-thiago <thiago.cardoso33@gmail.com>
2023-09-10 17:14:01 -0300

    Adicionando tratamento de erro na anotação e permitindo o uso a nível de método

--- logback-poc-app/src/main/kotlin/br/com/cardoso/controller/TestController.kt
+++ logback-poc-app/src/main/kotlin/br/com/cardoso/controller/TestController.kt
@@ -3,6 +3,7 @@ package br.com.cardoso.controller
 import br.com.cardoso.annotation.LogAop
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
+import java.lang.Exception
 
 @RestController
 @LogAop
@@ -13,7 +14,7 @@ class TestController {
     }
 
     @GetMapping("/test2")
-    fun test2(): String {
-        return "OK"
+    fun test2WithException(): String {
+        throw Exception("Erro na execução do test2")
     }
 }
--- logback-poc-app/src/main/kotlin/br/com/cardoso/controller/TestController2.kt
+++ logback-poc-app/src/main/kotlin/br/com/cardoso/controller/TestController2.kt
@@ -3,17 +3,20 @@ package br.com.cardoso.controller
 import br.com.cardoso.annotation.LogAop
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
+import java.lang.Exception
 
 @RestController
-@LogAop
 class TestController2 {
-    @GetMapping("/test")
-    fun test(): String {
+
+    @LogAop
+    @GetMapping("/test3")
+    fun test3MethodAnnotation(): String {
         return "OK"
     }
 
-    @GetMapping("/test2")
-    fun test2(): String {
-        return "OK"
+    @LogAop
+    @GetMapping("/test4")
+    fun test4MethodAnnotation(): String {
+        throw Exception("Erro na execução do test4")
     }
 }
