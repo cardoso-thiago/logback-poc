329f4e17496b2f99446dc6ea77ef3b99e0344c2b
cardoso-thiago <thiago.cardoso33@gmail.com>
2023-09-10 17:13:53 -0300

    Adicionando tratamento de erro na anotação e permitindo o uso a nível de método

--- logback-poc-aop-dep/src/main/java/br/com/cardoso/aspect/LogAspect.java
+++ logback-poc-aop-dep/src/main/java/br/com/cardoso/aspect/LogAspect.java
@@ -5,17 +5,45 @@ import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.Signature;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
 import org.slf4j.LoggerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Aspect
 public class LogAspect {
 
-    @Around("@within(br.com.cardoso.annotation.LogAop)")
+    @Pointcut("@annotation(br.com.cardoso.annotation.LogAop) || @within(br.com.cardoso.annotation.LogAop)")
+    public void isAnnotatedWithLogAop() {
+    }
+
+    @Around("isAnnotatedWithLogAop()")
     public Object logAop(ProceedingJoinPoint joinPoint) throws Throwable {
-        Object proceed = joinPoint.proceed();
+        long startTime = System.currentTimeMillis();
+        Object proceed;
+        try {
+            proceed = joinPoint.proceed();
+        } catch (Exception e) {
+            autoLog(joinPoint, startTime, e);
+            throw e;
+        }
+        autoLog(joinPoint, startTime, null);
+        return proceed;
+    }
+
+    private static void autoLog(ProceedingJoinPoint joinPoint, long startTime, Exception ex) {
+        long endTime = System.currentTimeMillis();
+        long executionTime = endTime - startTime;
         Signature signature = joinPoint.getSignature();
         String method = signature.getName();
-        LoggerFactory.getLogger(signature.getDeclaringType()).info(Markers.append("method", method), "");
-        return proceed;
+        Map<String, String> info = new HashMap<>();
+        info.put("method", method);
+        info.put("execution_time", executionTime + "ms");
+        if (ex == null) {
+            LoggerFactory.getLogger(signature.getDeclaringType()).info(Markers.appendEntries(info), "");
+        } else {
+            LoggerFactory.getLogger(signature.getDeclaringType()).error(Markers.appendEntries(info), "", ex);
+        }
     }
 }
---
+++ logback-poc-app/src/main/kotlin/br/com/cardoso/controller/TestController2.kt
@@ -0,0 +1,19 @@
+package br.com.cardoso.controller
+
+import br.com.cardoso.annotation.LogAop
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@LogAop
+class TestController2 {
+    @GetMapping("/test")
+    fun test(): String {
+        return "OK"
+    }
+
+    @GetMapping("/test2")
+    fun test2(): String {
+        return "OK"
+    }
+}
